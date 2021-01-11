063e4f9c3cfa6ea0e8fe09b147c97c5bd566e75b
Elena <74298374+Elenaesoler@users.noreply.github.com>
2021-01-11 17:25:29 +0100

    laboratorio

---
+++ TFG_Unity_Kinect/Kinect-BubbleGame-master/Assets/Scripts/Game/MeasureDepth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class MeasureDepth : MonoBehaviour
+{
+    public MultiSourceManager mMultiSource;
+
+    private CameraSpacePoint[] mCameraSpacePoints = null;
+    private ColorSpacePoint[] mcolorSpacePoints = null;
+
+
+    private KinectSensor mSensor = null;
+    private CoordinateMapper mMapper = null;
+
+    private readonly Vector2 mDepthResolution = new Vector2Int(512, 424);
+
+    private void awake()
+    {
+        mSensor = KinectSensor.GetDefault();
+        mMapper = mSensor.CoordinateMapper;
+
+        int arraySize = mDepthResolution.x * mDepthResolution.y;
+
+        mCameraSpacePoints = new CameraSpacePoint[arraySize];
+        mcolorSpacePoints = new ColorSpacePoint[arraySize];
+    }
+}
---
+++ TFG_Unity_Kinect/Kinect-BubbleGame-master/Assets/Scripts/Game/imageViewer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Windows.Kinect;
+
+
+public class imageViewer : MonoBehaviour
+{
+    public MultiSourceManager mMultiSource;
+
+    public RawImage mRawImage;
+
+    void Update()
+    {
+        mRawImage.texture = mMultiSource.GetColorTexture();
+
+    }
+}
